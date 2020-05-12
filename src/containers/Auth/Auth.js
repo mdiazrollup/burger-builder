358ca28110ac34afaebbb84ac90153743d0b3a80
Maria Diaz <mariangeles112@gmail.com>
2020-05-12 18:58:34 -0400

    optimisation and lazy loading

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,7 +6,8 @@ import * as actions from '../../store/actions/index';
 import {connect} from 'react-redux';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import {Redirect} from 'react-router-dom';
-
+import {updateObject, checkValidity} from '../../share/utility';
+ 
 class Auth extends Component {
 	state = {
 		controls: {
@@ -47,37 +48,14 @@ class Auth extends Component {
 		}
 	}
 
-	checkValidity(value, rules) {
-		let isValid = true;
-
-		if(!rules) {
-			return true;
-		}
-
-		if(rules.required) {
-			isValid = value.trim() !== '' && isValid;
-		}
-		if(rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if(rules.maxLength) {
-			isValid = value.length <= rules.maxLength && isValid;
-		}
-
-		return isValid;
-	}
-
 	inputChangedHandler = (event, inputIdentifier) => {
-		const updatedControls = {
-			...this.state.controls,
-			[inputIdentifier]: {
-				...this.state.controls[inputIdentifier],
+		const updatedControls = updateObject(this.state.controls,{
+			[inputIdentifier]: updateObject(this.state.controls[inputIdentifier], {
 				value: event.target.value,
-				valid: this.checkValidity(event.target.value, this.state.controls[inputIdentifier].validation),
+				valid: checkValidity(event.target.value, this.state.controls[inputIdentifier].validation),
 				touched: true
-			}
-		};
+			})
+		});
 
 		this.setState({controls: updatedControls});
 	}
